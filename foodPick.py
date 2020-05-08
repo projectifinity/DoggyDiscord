01c413bac8485ecb15a5a77bb6cd218be737281f
projectifinity <chastitybrooke01@gmail.com>
2020-05-08 09:02:56 +0000

    Initial commit

---
+++ foodPick.py
@@ -0,0 +1,11 @@
+import random
+
+def random_line(fname):
+
+  # finds out how many lines in txt file
+  lines = open(fname).read().splitlines()
+
+  while True:
+    return random.choice(lines)
+    
+#pickedFood = random_line('foodList.txt')
---
+++ main.py
@@ -0,0 +1,25 @@
+import discord
+import os #no idea what this is, but it works
+from keepAlive import keep_alive
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as', self.user)
+
+    async def on_message(self, message):
+        # don't respond to ourselves
+        if message.author == self.user:
+            return
+
+        if message.content == 'ping':
+            await message.channel.send('pong')
+
+        if message.content == 'w food':
+           from foodPick import random_line
+           await message.channel.send(random_line('foodList.txt'))
+
+client = MyClient()
+keep_alive()
+client.run(os.getenv("dtoken"))
+# no idea why "client.run('dtoken')" doesn't work but above line works
+
