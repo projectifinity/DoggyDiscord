9d96d532e245680adaae91c62bb3bdea081b1f2d
projectifinity <projectifinity@users.noreply.github.com>
2020-06-04 21:57:03 -0700

    testing "web server"

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import discord
 import os #no idea what this is, but it works
 from keepAlive import keep_alive
 
+
 class MyClient(discord.Client):
     async def on_ready(self):
         print('Logged on as', self.user)
 
     async def on_message(self, message):
-    # the "lower()" changes received argument to lowercase, so that the command will be recognized regardless of case
     
-    # don't respond to ourselves
+        # don't respond to ourselves
         if message.author == self.user:
             return
 
@@ -17,13 +17,14 @@ class MyClient(discord.Client):
             await message.channel.send('pong')
 
         #generates a "random" food item from list file
+        # the "lower()" changes received argument to lowercase, so that the command will be recognized regardless of case
         if message.content.lower() == 'w food':
             from foodPick import random_line
             await message.channel.send(random_line('foodList.txt'))
 
 
 client = MyClient()
-keep_alive()
+#keep_alive()
 client.run(os.getenv("dtoken"))
 # no idea why "client.run('dtoken')" doesn't work but above line works
 
